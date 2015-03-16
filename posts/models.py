7576a8ed888ad56d43fe2425dd928d308bc234ca
Firzatullah <fznoviar@gmail.com>
2015-03-16 12:08:15 +0700

    fixing posts app and create accounts app

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+from accounts.models import User
+
 class Post(models.Model):
-	title = models.CharField(max_length=255)
-	slug = models.SlugField(max_length=255)
-	body = models.TextField()
-	created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    body = models.TextField()
+    is_draft = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-	def __unicode__(self):
-		return self.title
+    user = models.ForeignKey(User)
 
-	class Meta:
-		verbose_name_plural = 'posts'
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'posts'
