8fbfbc6b2e40d64b35a872b42386c6ada0793104
Firzatullah <fznoviar@gmail.com>
2015-03-17 09:04:17 +0700

    fixing posts urls

--- suitblog/posts/urls.py
+++ suitblog/posts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('suitblog.posts.views',
-    url(r'^posts$', 'posts' name='posts'),
+    url(r'^posts$', 'posts', name='posts'),
 )
--- suitblog/urls.py
+++ suitblog/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'suitblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('suitblog.posts.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
